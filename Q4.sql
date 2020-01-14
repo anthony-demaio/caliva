29bd214ad630c64179baadca843e25a5cd77291a
anthony-demaio <59750920+anthony-demaio@users.noreply.github.com>
2020-01-13 22:46:50 -0500

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,11 +1,11 @@
-/**
+/*
  * Question 4
  * Among all posts with more than 1 answer, what are the top 1000 words used in the title?
  * 
  */
 
 
-with words_in_title as
+with words_in_title_array as
 (
 SELECT
 	id,
@@ -16,10 +16,21 @@ where creation_date >= '2019-08-01' and answer_count > 1
 order by id
 LIMIT 50000
 )
+,
+individual_words as (
 select 
 id, word
-from words_in_title 
-cross join UNNEST(words_in_title.word) as word;
+from words_in_title_array 
+cross join UNNEST(words_in_title_array.word) as word
+)
+select
+word,
+count(*) cnt
+from individual_words
+group by 
+word
+order by cnt desc 
+limit 1000
 
 select 
 words,
