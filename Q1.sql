2b6768f33923ebabc5491e3aefab9075dce44a77
anthony-demaio <59750920+anthony-demaio@users.noreply.github.com>
2020-01-14 19:49:12 -0500

    finalized version of answer to q1

--- Q1.sql
+++ Q1.sql
@@ -3,35 +3,46 @@
  * Is a more verbose description more likely to be answered?
  * 
  * Answer
- * Generally no. Based on the correlation of description length to total answers, it seems that for every additional character typed into a description, the question would receive 0.0287 fewer answers.
+ * No—a more verbose description is not more likely to be answered. Based on the correlation of description length to total answers overall, for every additional character typed into a description, the question would receive 0.0731 fewer answers.
  *What is the correlation (r^2) of the number of words (or letters) used in a description versus the number of answers
-*it gets, divided into quantiles?
-*Across the set of data queried, as defined by the WHERE predicate below, the pearson correlation coefficient (R) is -0.0287; whereas the R^2 is 0.001, indicating the length of description does a poor job of explaining the variability in the number of answers per questions.  The breakdown of both R and R^2 across the data broken into six quantiles is as follows: 
-*quantile	pearson_correlation_coefficient	R_squared
-q1_up_to_percentile10	0.068	0.0047
-q2_up_to_percentile25	0.026	0.0007
-q3_up_to_percentile50	0.023	0.0005
-q4_up_to_percentile75	-0.018	0.0003
-q5_up_to_percentile90	-0.020	0.0004
-q6_up_to_max	        -0.027	0.0007
+*it gets, divided into quantiles? 
+*Across the set of data queried, the pearson correlation coefficient (R) is -0.0731; whereas the R^2 (coefficient of determination) is 0.0053, indicating the length of description does a poor job of explaining the variability (only half a percent) in the number of answers per questions.  
 
-The pattern which clearly emerges is a decrease in number of answers as the length of the description becomes longer than the median length, to an increasingly sever degree as the descriptions approach the maximum length.
+The breakdown of several statistics:
+  —min_description_length_of_quantile
+  —avg_description_length_of_quantile
+  —max_description_length_of_quantile
+  —pearson_correlation_coefficient
+  —R_squared
+  —answers_per_quantile
+  —percent_of_total_answers
+
+Across the data broken into six quantiles is as follows:
+
+quantile	min_description_length_of_quantile	avg_description_length_of_quantile	max_description_length_of_quantile	pearson_correlation_coefficient	R_squared	answers_per_quantile
+q1_up_to_percentile10	17	233	328	-0.0591	0.0035	3,218,337
+q2_percentile10_up_to_percentile25	329	439	545	-0.0102	0.0001	4,497,778
+q3_percentile25_up_to_median	546	737	952	-0.0171	0.0003	7,205,836
+q4_median_up_to_percentile75	953	1,273	1,700	-0.0263	0.0007	6,736,176
+q5_percentile75_up_to_percentile90	1,701	2,212	2,990	-0.0246	0.0006	3,722,126
+q6_percentile90_up_to_max	2,991	5,684	58,431	-0.0244	0.0006	2,284,756
+
+Descriptions of a length falling into the third quantile, from 546 characters to 952 character (description lengths falling between the 25th percentile and the median (or 50th percentile)), received the greatest percentage of all answers. Broadly, the sweet spot for description length is between the second and fourth quantiles—just long enough, but not too long a description. Those three, of six, quantiles account for 67% of all answers.
  * 
- */with answers_by_length as
+ */
+with answers_by_length as
 (
 SELECT
 	id,
 	SUM(cast(length(body) as NUMERIC)) description_length,
-	countif(accepted_answer_id is not null) accepted_answer_count,
-	countif(accepted_answer_id is  null) No_accepted_answer_count,
 	sum(answer_count) total_answers
 FROM
 	`seismic-sweep-264823`.stackoverflow_caliva.posts_questions
-where creation_date >= '2019-08-01'
+--where creation_date >= '2019-08-01'
 group by
 id
-order by id
-LIMIT 50000
+--order by id
+--LIMIT 50000
 )
 ,
 stats as (
@@ -52,90 +63,108 @@ from
 	from
 		answers_by_length ) 
 	)
---select * from stats;
 /*
- * The query below returns the R and R^2 values, used to determine the correlation between length of description and answer count per question
- * 
+ * The query below returns the R and R^2 values across all data, used to determine the correlation between length of description and answer count per question.
+ */
+ --
+/*
 select
 CORR(total_answers, description_length) pearson_correlation_coefficient,
 POW(CORR(total_answers, description_length),2) R_squared,
 from
 answers_by_length;
 */
-	
+--
 /*
- * The query below calculates the R and R^2 values for each of six quantiles the data set is broken into.
+ * The query below calculates statistics for each of six quantiles the data set is broken into.
  */
+,
+quantile_stats as (
 select
 'q1_up_to_percentile10' as quantile,
+min(description_length) as min_description_length_of_quantile,
+avg(description_length) as avg_description_length_of_quantile,
+max(description_length) as max_description_length_of_quantile,
+sum(total_answers) answers_per_quantile,
 CORR(total_answers, description_length) pearson_correlation_coefficient,
-POW(CORR(total_answers, description_length),2) R_squared,
+POW(CORR(total_answers, description_length),2) R_squared
 from
 answers_by_length, stats
 where 
 description_length >= min and description_length <= percentile10
 union all
 select
-'q2_up_to_percentile25' as quantile,
+'q2_percentile10_up_to_percentile25' as quantile,
+min(description_length) as min_description_length_of_quantile,
+avg(description_length) as avg_description_length_of_quantile,
+max(description_length) as max_description_length_of_quantile,
+sum(total_answers) answers_per_quantile,
 CORR(total_answers, description_length) pearson_correlation_coefficient,
-POW(CORR(total_answers, description_length),2) R_squared,
+POW(CORR(total_answers, description_length),2) R_squared
 from
 answers_by_length, stats
 where 
 description_length > percentile10 and description_length <= percentile25
 union all 
 select
-'q3_up_to_percentile50' as quantile,
+'q3_percentile25_up_to_median' as quantile,
+min(description_length) as min_description_length_of_quantile,
+avg(description_length) as avg_description_length_of_quantile,
+max(description_length) as max_description_length_of_quantile,
+sum(total_answers) answers_per_quantile,
 CORR(total_answers, description_length) pearson_correlation_coefficient,
-POW(CORR(total_answers, description_length),2) R_squared,
+POW(CORR(total_answers, description_length),2) R_squared
 from
 answers_by_length, stats
 where 
 description_length > percentile25 and description_length <= median
 union all 
 select
-'q4_up_to_percentile75' as quantile,
+'q4_median_up_to_percentile75' as quantile,
+min(description_length) as min_description_length_of_quantile,
+avg(description_length) as avg_description_length_of_quantile,
+max(description_length) as max_description_length_of_quantile,
+sum(total_answers) answers_per_quantile,
 CORR(total_answers, description_length) pearson_correlation_coefficient,
-POW(CORR(total_answers, description_length),2) R_squared,
+POW(CORR(total_answers, description_length),2) R_squared
 from
 answers_by_length, stats
 where 
 description_length > median and description_length <= percentile75
 union all 
 select
-'q5_up_to_percentile90' as quantile,
+'q5_percentile75_up_to_percentile90' as quantile,
+min(description_length) as min_description_length_of_quantile,
+avg(description_length) as avg_description_length_of_quantile,
+max(description_length) as max_description_length_of_quantile,
+sum(total_answers) answers_per_quantile,
 CORR(total_answers, description_length) pearson_correlation_coefficient,
-POW(CORR(total_answers, description_length),2) R_squared,
+POW(CORR(total_answers, description_length),2) R_squared
 from
 answers_by_length, stats
 where 
 description_length > percentile75 and description_length <= percentile90
 union all 
 select
-'q6_up_to_max' as quantile,
+'q6_percentile90_up_to_max' as quantile,
+min(description_length) as min_description_length_of_quantile,
+avg(description_length) as avg_description_length_of_quantile,
+max(description_length) as max_description_length_of_quantile,
+sum(total_answers) answers_per_quantile,
 CORR(total_answers, description_length) pearson_correlation_coefficient,
-POW(CORR(total_answers, description_length),2) R_squared,
+POW(CORR(total_answers, description_length),2) R_squared
 from
 answers_by_length, stats
 where 
 description_length > percentile90 and description_length <= max
+)
+select
+quantile,
+min_description_length_of_quantile,
+avg_description_length_of_quantile,
+max_description_length_of_quantile,
+pearson_correlation_coefficient, R_squared, answers_per_quantile,
+answers_per_quantile * 1.0 / (select sum(total_answers) from answers_by_length) as percent_of_total_answers
+from
+quantile_stats
 order by quantile;
-
-
------
-SELECT
-sum(if((description_length >= min and description_length <= percentile10),total_answers,0)) q1_up_to_percentile10,
-sum(if((description_length > percentile10 and description_length <= percentile25),total_answers,0)) q2_up_to_percentile25,
-sum(if((description_length > percentile25 and description_length <= median),total_answers,0)) q3_up_to_percentile50,
-sum(if((description_length > median and description_length <= percentile75),total_answers,0)) q4_up_to_percentile75,
-sum(if((description_length > percentile75 and description_length <= percentile90),total_answers,0)) q5_up_to_percentile90,
-sum(if((description_length > percentile90 and description_length <= max),total_answers,0)) q6_up_to_max
-from 
-answers_by_length,
-stats
-;
-
-SELECT
-	COUNT(id)
-FROM
-	`seismic-sweep-264823`.stackoverflow_caliva.posts_questions;
